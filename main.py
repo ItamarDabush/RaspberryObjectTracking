d67a50f414c08784f281cc81aed93bb842458dab
Eran And Itamar <EranAndItamar@gmail.com>
2021-10-01 12:12:27 +0100

    dabush doing shit

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+from picamera import PiCamera
+from time import sleep
+
 def main():
-  print("hello")
-
-
-if __name__ == "main":
+  print("Start Running")
+  camera = PiCamera()
+  camera.start_preview()
+  sleep(5)
+  camera.stop_preview()
+if __name__ == "__main__":
   main()
