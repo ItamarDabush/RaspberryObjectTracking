7e8af0b6c4fe1fabebd9f0279e69cfb3c31c6e76
ItamarDabush <itamar868@gmail.com>
2021-12-15 12:22:17 +0000

    add mark detected object func

--- project/main.py
+++ project/main.py
@@ -1,5 +1,12 @@
 import cv2
+import sys
+sys.path.append("Desktop/project")
 from project.VideoStream import VideoStream
+
+def mark_detected_object(frame, center, width, height):
+    first_corner = (center[0]-width/2, center[1]-height/2)
+    opposite_corner = (center[0]+width/2, center[1]+height/2)
+    cv2.rectangle(frame, first_corner, opposite_corner, (0,255,0), thickness=2)
 
 
 def on_new_frame(frame):
